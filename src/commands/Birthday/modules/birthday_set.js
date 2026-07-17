6c5fe39ec578a3cd5da90bc34fa6dce7052228df
daniIochka <dunchovy@gmail.com>
2026-07-17 16:10:01 +0700

    Translate birthday set command to Russian

--- src/commands/Birthday/modules/birthday_set.js
+++ src/commands/Birthday/modules/birthday_set.js
@@ -2,12 +2,13 @@ import { EmbedBuilder } from 'discord.js';
 import { setBirthday } from '../../../services/birthdayService.js';
 
 import { InteractionHelper } from '../../../utils/interactionHelper.js';
+
 export default {
     async execute(interaction, config, client) {
         await InteractionHelper.safeDefer(interaction);
 
-        const month = interaction.options.getInteger("month");
-        const day = interaction.options.getInteger("day");
+        const month = interaction.options.getInteger("месяц");
+        const day = interaction.options.getInteger("день");
         const userId = interaction.user.id;
         const guildId = interaction.guildId;
 
@@ -15,8 +16,8 @@ export default {
 
         const embed = new EmbedBuilder()
             .setColor(0x00FF00)
-            .setTitle('Birthday Set!')
-            .setDescription(`Your birthday has been set to **${result.data.monthName} ${result.data.day}**!`);
+            .setTitle('🎉 День рождения установлен!')
+            .setDescription(`🎂 Ваш день рождения установлен на **${result.data.monthName} ${result.data.day}**!`);
 
         await InteractionHelper.safeEditReply(interaction, {
             embeds: [embed]
